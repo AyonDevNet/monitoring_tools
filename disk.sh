a9611135ce9799c988ff4f97a158e649a1a03b8b
AyonDevNet <rmcoder7@gmail.com>
2025-04-30 15:50:37 +0600

    add

--- disk.sh
+++ disk.sh
@@ -1,11 +1,12 @@
 #Create a Hard disk Monitoring tools
 
 disk_Space=$(df -H | egrep -v "Filesystem|temps" | grep "sda2" | awk '{print $4}' | tr -d %)
+TO="example@gmail.com"
 
 if (( disk_Space == 80 )); then
      echo "this disk is good"
 else
-    echo "Disk usage is exactly 80% - Warning level reached"
+    echo "Disk usage is exactly 80% - Warning level reached - $disk_Space %" | mail -s "this alert for disk fullage !!" $TO
 fi
 
 
